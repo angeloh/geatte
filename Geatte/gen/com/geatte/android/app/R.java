ae78c059f1c1a5e99bb9af0599e62e4285473bbf
angelo k. huang <angelokh@ymail.com>
2011-06-10 00:15:09 -0700

    fix bg of comment box

--- Geatte/gen/com/geatte/android/app/R.java
+++ Geatte/gen/com/geatte/android/app/R.java
@@ -695,6 +695,7 @@ containing a value of this type.
         public static final int yes_label=0x7f01004c;
     }
     public static final class color {
+        public static final int dark_gray=0x7f060008;
         public static final int gd_action_bar_divider_tint=0x7f060001;
         public static final int gd_action_bar_tint=0x7f060000;
         public static final int light_blue=0x7f060007;
